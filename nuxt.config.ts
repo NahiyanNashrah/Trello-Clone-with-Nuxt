57e01241459c0a07592c0b38c9a94e33cb536160
Nahiyan Nasrah <nahiyan12@student.sust.edu>
2024-05-17 13:17:32 +0600

    UI done

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@pinia/nuxt']
+  css: [
+    '~/assets/css/vello.css',
+  ],
+  modules: [
+    '@nuxt/ui', 
+    '@pinia/nuxt', 
+  ],
 })
